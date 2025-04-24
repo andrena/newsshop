04d1a75c455779fa0f1ea19b1edb916e09c21a09
Christian Jansohn <christian.jansohn@andrena.de>
2025-04-24 11:36:28 +0200

    add interaction to test

--- frontend/src/subscribe/__tests__/Subscribe.spec.js
+++ frontend/src/subscribe/__tests__/Subscribe.spec.js
@@ -1,13 +1,34 @@
 // Subscribe.spec.js
-import { render, screen } from '@testing-library/vue'
+import {fireEvent, render, screen} from '@testing-library/vue'
+import axios from 'axios';
 import Subscribe from '@/subscribe/Subscribe.vue'
+
+jest.mock('axios');
 
 describe('Subscribe.vue', () => {
 
-    test('renders correctly', () => {
+    test('should be able to add new subscription', async () => {
+        axios.post.mockResolvedValueOnce(
+            {
+                data: {
+                    email: 'test@mail.de',
+                    name: 'name',
+                    source: 'Webseite'
+                }
+            })
+
         render(Subscribe)
 
         expect(screen.getByText('Newsletter Subscription')).toBeInTheDocument();
+
+        expect(screen.getByLabelText('E-Mail:')).toBeInTheDocument();
+        await fireEvent.update(screen.getByLabelText('E-Mail:'), "test@mail.de");
+        await fireEvent.update(screen.getByLabelText('Name:'), "my name");
+        await fireEvent.update(screen.getByLabelText('Quelle:'), "Website");
+
+        await fireEvent.click(screen.getByRole('button', {name: 'Abonnieren'}));
+
+        expect(await screen.findByText('Subscription Successful')).toBeInTheDocument();
     })
 
 })
